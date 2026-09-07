76fd72be52d49613984e415fc45e3d14890fa9f8
Prashant-Mathwale <prashant.g.mathwale@gmail.com>
2026-09-07 20:45:10 +0530

    Add Node.js and Express setup

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+
+const PORT = 3000;
+
+// Serve static files
+app.use(express.static(__dirname));
+
+// Start server
+app.listen(PORT, () => {
+    console.log(`Student Task Manager running at http://localhost:${PORT}`);
+});
